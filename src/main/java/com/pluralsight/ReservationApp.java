2ece669d2fa3320a0e637fb86406c2f6cd4f696b
Erikarod27 <rodriguez.erika321@gmail.com>
2024-04-16 14:09:54 -0400

    Finished Theatre Reservations Application

---
+++ src/main/java/com/pluralsight/ReservationApp.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class ReservationApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        //get name, date, and tickets as input
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine().trim();
+        System.out.print("What date will you be coming (mm/dd/yyyy): ");
+        String dateString = scanner.nextLine().trim();
+        //format date
+        String formattedDate = LocalDate.parse(dateString, formatter).format(formatter);
+        System.out.print("How many tickets would you like? ");
+        int tickets = scanner.nextInt();
+        //split name into nameParts array to rename values
+        String[] nameParts = name.split("\\s+");
+        String lastName = nameParts[1];
+        String firstName = nameParts[0];
+        //print ticket for single ticket and tickets for multiple tickets
+        if (tickets == 1) {
+            System.out.printf("%d Ticket reserved for %s under %s, %s", tickets, formattedDate, lastName, firstName);
+        } else {
+            System.out.printf("%d Tickets reserved for %s", tickets, formattedDate);
+        }
+
+    }
+}
